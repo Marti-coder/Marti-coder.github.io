b40c69454c7f4144bf3b0ffb16a6e30a20a6a83f
Marti-coder <73259886+Marti-coder@users.noreply.github.com>
2022-07-30 18:06:21 +0200

    Update aboutEN.js

--- ABOUT EN/aboutEN.js
+++ ABOUT EN/aboutEN.js
@@ -4,9 +4,9 @@ const text = document.querySelector(".text-wrapp");
 setTimeout(function() {
     background.style.marginRight = "400px";
     background.style.opacity = "1";
-    text.style.display = "block";
 
-}, 2000);
+
+}, 4000);
 
 //nav-list appearance 
 const navbtn = document.querySelector(".nav-toggle-btn");
@@ -15,5 +15,5 @@ const navList = document.querySelector(".nav-list");
 navbtn.addEventListener("click", function() {
     navList.classList.toggle("show-nav-list");
     document.querySelector("html").classList.toggle("hidde");
-    
+
 })
