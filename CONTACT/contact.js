d9b4d49bb587e9fdd903f95bcc5d75e3feba1d53
Marti-coder <73259886+Marti-coder@users.noreply.github.com>
2022-07-31 15:18:15 +0200

    Update contact.js

--- CONTACT/contact.js
+++ CONTACT/contact.js
@@ -2,27 +2,27 @@ const video = document.querySelector(".video video");
 const otherSide = document.querySelector(".other-side");
 const wrapper = document.querySelector(".wrapper");
 const formWrapper = document.querySelector(".form-wrapper");
-
+/*
 video.addEventListener("mouseover", mOver, false);
 wrapper.addEventListener("mouseout", mOut, false);
 
 function mOver() {
     video.style.display = "none";
-    otherSide.style.display = "flex"; 
- }
- 
- function mOut() {  
+    otherSide.style.display = "flex";
+}
+
+function mOut() {
     otherSide.style.display = "none";
     video.style.display = "block";
- }
+}
 
- //nav-list appearance 
+*/
+//nav-list appearance 
 const navbtn = document.querySelector(".nav-toggle-btn");
 const navList = document.querySelector(".nav-list");
 
 navbtn.addEventListener("click", function() {
     navList.classList.toggle("show-nav-list");
     document.querySelector("html").classList.toggle("hidde");
-    
-})
 
+})
